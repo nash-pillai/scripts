b22384eaef179151fa28ff8001f934bd6af82adb
Nash Pillai <nash.pillai@protonmail.com>
2025-07-05 17:47:46 -0400

    update setup script with new dirs and links

--- install/setup.sh
+++ install/setup.sh
@@ -2,28 +2,38 @@
 
 cd ~
 
+git clone https://github.com/nash-pillai/config.git .config
+mkdir -p .local/scripts
+git clone https://github.com/nash-pillai/scripts.git .local/scripts
+
 # Links
 ln -s .config/zsh/.zshenv .zshenv
-ln -s .data/audacity-data .audacity-data
 ln -s .config/cups .cups
-ln -s .data/eocvsim .eocvsim
-ln -s .data/googleearth .googleearth
 ln -s .data/minecraft .minecraft
-ln -s .data/floorp .mozilla
+ln -s .data/floorp .floorp
 ln -s .data/ssh .ssh
 ln -s .data/thunderbird .thunderbird
 
-mkdir -p ~/.local/Apps/ ~/Documents/ ~/Tmp/
+# Create directories
+mkdir -p ~/Documents/ ~/Documents/Public/ ~/Tmp/
 mkdir -p ~/Media/Music/ ~/Media/Pictures/ ~/Media/Videos/ ~/Media/Screenshots/
+xdg-user-dirs-update
 
-chmod +x ~/.local/Apps/*
-ln -sr ~/.local/Apps/* ~/.local/bin
+config_links=('Badlion Client' cef_user_data Element Insomnia mozc vivaldi BambuStudio chromium launcher obsidian Bitwarden discord heroic libreoffice Slack)
+home_links=('minecraft' 'floorp' 'ssh' 'thunderbird')
+for dir in "${config_links[@]}" "${home_links[@]}"; do
+    mkdir -p ~/.data/$dir
+done
 
-exit
+sudo mkdir -p /Media/Wallpapers/ /Media/Code/
+sudo chown -R nash:users /Media/
+git clone https://github.com/nash-pillai/wallpapers.git /Media/Wallpapers/
+
 
 cd /Media/Code
 
 git clone --depth 1 https://github.com/hlissner/doom-emacs emacs
 doom install
 
-git clone https://github.com/nash-pillai/tedi.git
+git clone https://github.com/nash-pillai/stapplet.git
+git clone https://github.com/nash-pillai/stapplet-new.git
